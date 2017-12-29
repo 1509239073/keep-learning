7bd551c644da68b04b178d507b5efa1e3e7807ea
taixing <1509239073@qq.com>
2017-12-29 10:20:15 +0800

    url配置

--- testdj/testdj/urls.py
+++ testdj/testdj/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/index/',views.index),
+    # url(r'^blog/index/\d{2}$',views.index),
+    url(r'^blog/index/(?P<id>\d{2})/$',views.index),
 ]
